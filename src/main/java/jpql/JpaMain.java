fde5df52ba7869f8c3c97aef9ab6881e13818d69
gyunny <xorbs15923@naver.com>
2022-11-21 13:54:21 +0900

    ORM-JPA jpql select 프로젝션

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,6 +1,8 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.Collection;
+import java.util.List;
 
 public class JpaMain {
 
@@ -17,10 +19,15 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
-                    .setParameter("username", "member1")
-                    .getSingleResult();
-            System.out.println("result = " + result.getUsername());
+            em.flush();
+            em.clear();
+
+            List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+                    .getResultList();
+
+            MemberDTO memberDTO = result.get(0);
+            System.out.println("memberDTO = " + memberDTO.getUsername());
+            System.out.println("memberDTO = " + memberDTO.getAge());
 
             tx.commit();
         } catch (Exception e) {
