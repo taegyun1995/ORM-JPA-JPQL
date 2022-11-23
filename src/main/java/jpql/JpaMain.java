1b1e3330982ea2e83cd8e5ec3b1076ad2eb09095
gyunny <xorbs15923@naver.com>
2022-11-23 16:51:52 +0900

    ORM-JPA 조건식

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -19,7 +19,7 @@ public class JpaMain {
             em.persist(team);
 
             Member member = new Member();
-            member.setUsername("teamA");
+            member.setUsername("관리자");
             member.setAge(10);
             member.setType(MemberType.ADMIN);
             member.setTeam(team);
@@ -28,16 +28,24 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select m.username, 'HELLO', TRUE from Member m" +
-                    " where m.type = :userType";
-            List<Object[]> result = em.createQuery(query)
-                    .setParameter("userType", MemberType.ADMIN)
+//            String query =
+//                    "select " +
+//                            "case when m.age <= 10 then '학생요금'" +
+//                            "       when m.age >= 60 then '학생요금'" +
+//                            "       else '일반요금' " +
+//                            "end " +
+//                            " from Member m";
+
+//            String query = "select coalesce(m.username, '이름 없는 회원') as username " +
+//                    "from Member m";
+            String query = "select nullif(m.username, '관리자') as username " +
+                    "from Member m";
+
+            List<String> result = em.createQuery(query, String.class)
                     .getResultList();
 
-            for (Object[] objects : result) {
-                System.out.println("objects = " + objects[0]);
-                System.out.println("objects = " + objects[1]);
-                System.out.println("objects = " + objects[2]);
+            for (String s : result) {
+                System.out.println("s = " + s);
             }
 
             tx.commit();
